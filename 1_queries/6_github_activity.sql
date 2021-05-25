f1002d9ad13685c0f298562567922e297eaaec56
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-24 23:52:11 -0400

    creating a folder for queries related to join, also adding 2 join queries that is using 2 or more tables

--- 1_queries/6_github_activity.sql
+++ 1_queries/6_github_activity.sql
@@ -2,3 +2,9 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL
 AND end_date IS NOT NULL;
+
+SELECT students.name AS student_name, cohorts.name AS cohort_name, 
+cohorts.start_date AS cohort_start_date, students.start_date as student_start_date
+FROM students LEFT JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohorts.start_date;
